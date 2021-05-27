c7512ffe75d9ad9d53a8f97509b04a957c8412d2
anishch <43155397+anishch@users.noreply.github.com>
2021-05-27 01:59:28 -0700

    added details to Event.java

--- app/src/main/java/com/example/finalproject/Event.java
+++ app/src/main/java/com/example/finalproject/Event.java
@@ -1,7 +1,66 @@
 package com.example.finalproject;
 
-public class Event {
-    public Event(){
+import android.location.Location;
 
+import java.sql.Time;
+import java.util.Date;
+import java.util.UUID;
+
+public class Event {
+    //figured out how to clone, commit and push
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Time mTime;
+    private boolean mCompleted;
+    private Location mLocation;
+
+    public Event() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+        mTime = new Time(mDate.getTime());
+
+    }
+
+    public void setTime(int hours, int mins, int seconds){
+        this.mTime = new Time(hours, mins, seconds);
+    }
+
+    public Location getLocation(){
+        return mLocation;
+    }
+
+    /*public Location setLocation(){
+        //
+    }*/
+
+    public Time getTime(){
+        return mTime;
+    }
+    public void setTime(long time){
+        this.mTime.setTime(time);
+        this.mDate.setTime(time);
+    }
+    
+    public UUID getId() {
+        return mId;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+    public Date getDate() {
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+    public void setCompleted(boolean completed) {
+        mCompleted = completed;
     }
 }
